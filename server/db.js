5084612b431b1d254706c25b1013dacbf87496d7
Jackie Lai <jlai723@gmail.com>
2021-06-15 14:43:02 -0400

    Minor edit to postgresql passcode due to new computer

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize("postgres://postgres:718d2915e10f4cb5b85c9393b2d58259@localhost:5432/eleven-journal");
+const sequelize = new Sequelize("postgres://postgres:afd60085930c46f0923426a39dc78dfc@localhost:5432/eleven-journal");
 
 module.exports = sequelize;
